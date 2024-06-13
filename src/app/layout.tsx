3941f801873581383f0e5cc7c268142ebcd93e8c
jdboachie <jdboachie@gmail.com>
2024-06-13 19:46:28 +0000

    add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
+import { SignIn, SignOut } from "@/components/auth/components";
+import { auth } from "@/auth";
 
 
-export default function Home() {
+export default async function Home () {
+
+  const session = await auth()
   return (
-    <main>
-      coming soon
+    <main className="w-screen h-screen grid items-center justify-center">
+      { session?.user &&
+      <div className="flex flex-col gap-4 p-4 w-full bg-gray-100 rounded-md">
+        <h2 className="text-xl font-bold">Current Session Data</h2>
+        {Object.keys(session.user).length > 3 ? (
+          <p>
+            In this example, the whole session object is passed to the page,
+            including the raw user object. Our recommendation is to{" "}
+            <em>only pass the necessary fields</em> to the page, as the raw user
+            object may contain sensitive information.
+          </p>
+        ) : (
+          <p>
+            In this example, only some fields in the user object is passed to
+            the page to avoid exposing sensitive information.
+          </p>
+        )}
+        <div className="flex flex-col rounded-md bg-neutral-100">
+          <div className="p-4 font-bold rounded-t-md bg-neutral-200">
+            Session
+          </div>
+          <pre className="py-6 px-4 whitespace-pre-wrap break-all">
+            {JSON.stringify(session, null, 2)}
+          </pre>
+        </div>
+      </div> }
+      {session?.user ? <SignOut /> : <SignIn />}
     </main>
   );
 }
