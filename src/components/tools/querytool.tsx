66153dde085b4a6d103ec986b1cddbfa87ca57aa
jdboachie <jdboachie@gmail.com>
2024-07-13 21:59:14 +0000

    sorry: gotta keep pushing because of my crappy laptop

--- src/components/tools/querytool.tsx
+++ src/components/tools/querytool.tsx
@@ -212,7 +212,7 @@ const QueryTool = () => {
           </div>
           <div className="h-[calc(100%-86px)]">
             {outputData ? (
-              <ScrollArea className={`text-sm flex flex-col-reverse place-items-center font-mono tracking-normal w-[calc(100%-1px)] h-[calc(100%-86px)]`}>
+              <ScrollArea className={`text-sm flex flex-col-reverse place-items-center font-mono tracking-normal w-[calc(100%-1px)] h-[calc(100%-33px)]`}>
                 <table className='table-auto w-fit h-fit text-left border-collapse transition-all duration-300 ease-in-out'>
                   <thead className='sticky top-[-1px] bg-primary-foreground drop-shadow max-h-[1rem] min-h-[1rem]'>
                     <tr className='truncate'>
