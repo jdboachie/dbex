fa913278874ec44351e6b41155bff2881386e29e
gyauelvis <gyauelvis@gmail.com>
2024-07-23 02:57:31 +0000

    login for sending mail

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -126,3 +126,19 @@ export const Analytics = async () => {
 
   return { queries, connection };
 };
+
+export const SendMail = async (email: string, message: string) => {
+  const apiEndpoint = '/api/email';
+
+  fetch(apiEndpoint, {
+    method: 'POST',
+    body: JSON.stringify({email,message}),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      alert(response.message);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+};
